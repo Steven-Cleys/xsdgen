e6c1d1b94b87179db1d7e2d0ecb2ced44a58345d
cleysst <steven.cleys@rmconsulting.be>
2016-10-07 16:24:02 +0200

    messy but works for now
    messy but works for now

--- src/main/java/be/devoowi/controller/FileUploadController.java
+++ src/main/java/be/devoowi/controller/FileUploadController.java
@@ -5,6 +5,11 @@ import java.nio.charset.StandardCharsets;
 import java.util.logging.Logger;
 
 import be.devoowi.xsdgenerator.GenerateXsdFromBiSampleCode;
+import org.apache.commons.io.IOUtils;
+import org.primefaces.event.FileUploadEvent;
+import org.primefaces.model.DefaultStreamedContent;
+import org.primefaces.model.StreamedContent;
+import org.primefaces.model.UploadedFile;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -12,26 +17,31 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.ExternalContext;
+import javax.faces.context.FacesContext;
 import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletResponse;
 
+@ManagedBean
 @Controller
+@WebServlet
 public class FileUploadController {
 
 	private static final Logger logger = Logger.getLogger(FileUploadController.class.toString());
+	private InputStream xsdgen;
 
+	//private MultipartFile file;
 	/**
 	 * Upload single file using Spring Controller
 	 */
-	@RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
-	public @ResponseBody
-	void uploadFileHandler(
-			@RequestParam("file") MultipartFile file,
-							 HttpServletResponse response) {
 
-		if (!file.isEmpty()) {
+	public void uploadFileHandler(FileUploadEvent event) {
+
 			try {
-				byte[] bytes = file.getBytes();
+				UploadedFile file = event.getFile();
+				InputStream myInputStream = file.getInputstream();
 
 				// Creating the directory to store file
 				String rootPath = System.getProperty("catalina.home");
@@ -41,10 +51,11 @@ public class FileUploadController {
 
 				// Create the file on server
 				File serverFile = new File(dir.getAbsolutePath()
-						+ File.separator + file.getOriginalFilename());
+						+ File.separator + file.getFileName());
 				BufferedOutputStream stream = new BufferedOutputStream(
 						new FileOutputStream(serverFile));
-				stream.write(bytes);
+				stream.write(IOUtils.toByteArray(myInputStream));
+
 				stream.close();
 
 				logger.info("Server File Location="
@@ -52,30 +63,43 @@ public class FileUploadController {
 
 				GenerateXsdFromBiSampleCode gen = new GenerateXsdFromBiSampleCode();
 
-				InputStream xsdgen = new ByteArrayInputStream(gen.run(serverFile.getAbsolutePath()).getBytes(StandardCharsets.UTF_8));
-				response.setContentType("text/plain");
+				xsdgen = new ByteArrayInputStream(gen.run(serverFile.getAbsolutePath()).getBytes(StandardCharsets.UTF_8));
+				HttpServletResponse response =
+						(HttpServletResponse) FacesContext.getCurrentInstance()
+								.getExternalContext().getResponse();
+
+				response.setContentType("application/octet-stream");
 				response.setHeader("Content-Disposition", "attachment; filename=autogen.xsd");
-				OutputStream out = response.getOutputStream();
-				byte [] buffer = new byte[256];
+				//OutputStream out = response.getOutputStream();
+				byte [] buffer = new byte[1024];
 				int bytesRead = 0;
+				ByteArrayOutputStream out = new ByteArrayOutputStream();
 				while((bytesRead = xsdgen.read(buffer)) != -1)
 				{
+
 					out.write(buffer, 0, bytesRead);
 				}
-				out.flush();
 				out.close();
-
-
-				//return "dsf";
+				response.getOutputStream().write(out.toByteArray());
+				response.getOutputStream().flush();
+				response.getOutputStream().close();
+				getDownload();
+				FacesContext.getCurrentInstance().responseComplete();
 
 
 			} catch (Exception e) {
 				//return "You failed to upload " + name + " => " + e.getMessage();
 			}
-		} else {
-			//return "You failed to upload " + name
-			//		+ " because the file was empty.";
-		}
+	}
+
+	public StreamedContent getDownload() throws Exception {
+		StreamedContent download=new DefaultStreamedContent();
+		File file = new File("C:\\Users\\cleysst\\workspace\\xsdgen\\null\\tmpFiles\\XXKPMG_EMPLOYEES_PART_2_CODE.xml.xsd");
+		InputStream input = new FileInputStream(file);
+		ExternalContext externalContext = FacesContext.getCurrentInstance().getExternalContext();
+		download = new DefaultStreamedContent(input, externalContext.getMimeType(file.getName()), file.getName());
+		System.out.println("PREP = " + download.getName());
+		return download;
 	}
 
 	/**
